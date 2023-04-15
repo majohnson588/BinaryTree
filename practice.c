0c7910d8cc8fd0926f8be6be300ab463ec40e381
majohnson588 <117084422+majohnson588@users.noreply.github.com>
2023-04-16 00:48:37 +0800

    Update practice.c

--- practice.c
+++ practice.c
@@ -54,3 +54,160 @@ void TestTopk()
 	a[3144] = 1000000 + 10;
 	PrintTopK(a, n, 10);
 }
+
+//判断是否为单值二叉树
+//如果二叉树每个节点都具有相同的值，那么该二叉树就是单值二叉树
+#include "Heap.h"
+#include "test.c"
+
+bool flag = true;
+void PreOrderCompare(struct TreeNode* root, int val)
+{
+	if (root == NULL || flag == false)
+		return;
+
+	if (root->val != val)
+	{
+		flag = false;
+		return;
+	}
+
+	PreOrderCompare(root->left, val);
+	PreOrderCompare(root->right, val);
+}
+
+bool inUnivalTree1(struct TreeNode* root) {
+	if (root == NULL)
+		return true;
+
+	flag = true;
+	PreOrderCompare(root, root->val);
+	return flag;
+}
+
+bool inUnivalTree2(struct TreeNode* root) {
+	if (root == NULL)
+		return true;
+
+	if (root->left && root->left->val != root->val)
+		return false;
+
+	if (root->right && root->right->val != root->val)
+		return false;
+
+	return isUnivalTree(root->left)
+		&& isUnivalTree(root->right);
+}
+
+//相同的树
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+	if (p == NULL && q == NULL)
+		return true;
+
+	if (p == NULL || q == NULL)
+		return false;
+
+	if (p->val != q->val)
+		return false;
+
+	return isSameTree(p->left, q->left)
+	&& isSameTree(p->right, q->right);
+}
+
+bool isSymmetricSubTree(struct TreeNode* root1, struct TreeNode* root2)
+{
+	if (root1 == NULL && root2 == NULL)
+		return true;
+	
+	if (root1 == NULL || root2 == NULL)
+		return false;
+
+	if (root1->val != root2->val)
+		return false;
+
+	return isSymmetricSubTree(root1->left, root2->right)
+		&& isSymmetricSubTree(root1->right, root2->left);
+}
+
+//对称的树
+bool isSymmetric(struct TreeNode* root) {
+	if (root == NULL)
+		return true;
+
+	return isSymmetricSubTree(root->left, root->right);
+}
+
+//判断是否为子树
+bool isSubtree(struct TreeNode* root, struct TreeNode* subRoot)
+{
+	if (root == NULL)
+		return false;
+
+	//遍历、跟root中所有子树都比较一遍
+	if (isSameTree(root, subRoot))
+		return true;
+
+	return isSubtree(root->left, subRoot)
+		|| isSubtree(root->right, subRoot);
+}
+
+int TreeSize(struct TreeNode* root)
+{
+	return root == NULL ? 0 : TreeSize(root->left) + TreeSize(root->right) + 1;
+}
+
+//前序遍历
+
+void preorder(struct TreeNode* root, int* a, int* pi)
+{
+	if (root == NULL)
+		return;
+
+	a[(*pi)++] = root->val;
+	preorder(root->left, a, pi);
+	preorder(root->right, a, pi);
+}
+
+int* preorderTraversal(struct TreeNode* root, int* returnSize) {
+	*returnSize = TreeSize(root);
+	int* a = (int*)malloc(*returnSize * sizeof(int));
+	int i = 0;
+	preorder(root, a, &i);
+
+	return a;
+}
+
+//根据字符串建立二叉树并遍历
+BTNode* CreateTree(char* str, int* pi)
+{
+	if (str[*pi] == '#')
+	{
+		(*pi)++;
+		return NULL;
+	}
+
+	BTNode* root = BuyNode(str[(*pi)++]);
+	root->left = CreateTree(str, pi);
+	root->right = CreateTree(str, pi);
+	return root;
+}
+
+void InOrder(BTNode* root)
+{
+	if (root == NULL)
+		return;
+	InOrder(root->left);
+	printf("%c ", root->data);
+	InOrder(root->right);
+}
+
+int main()
+{
+	char str[100];
+	scanf("%s ", str);
+	int i = 0;
+	BTNode* root = CreateTree(str, &i);
+	InOrder(root);
+
+	return 0;
+}
